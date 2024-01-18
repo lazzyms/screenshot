96ed9c240fd735bb65d567aad4aec88528745c18
Maulik Sompura <maulik.sompura06@gmail.com>
2024-01-18 19:52:20 +0530

    update crop condition

--- src/paintVideoFrameOnCanvas.ts
+++ src/paintVideoFrameOnCanvas.ts
@@ -10,13 +10,18 @@ export const paintVideoFrameOnCanvas = (
     // @ts-ignore because getTracks is very much valid in modern browsers
     const videoTrackSettings = video.srcObject?.getTracks()[0].getSettings();
     // Create a canvas with the video's size and draw the video frame on it
+
     const canvas = document.createElement("canvas");
     canvas.width = width ?? videoTrackSettings?.width ?? 0;
     canvas.height = height ?? videoTrackSettings?.height ?? 0;
     const ctx = canvas.getContext("2d");
     x = x ?? 0;
     y = y ?? 0;
-    ctx?.drawImage(video, x, y);
+    if (width && height) {
+      ctx?.drawImage(video, x, y, width, height, 0, 0, width, height);
+    } else {
+      ctx?.drawImage(video, x, y);
+    }
     return canvas;
   } catch (err) {
     throw err;
